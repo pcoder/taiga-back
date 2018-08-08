6c84869299ea30553021e3ff558b032b6cd46ff3
M.Ravi <mondi.ravi@gmail.com>
2018-08-08 21:19:05 +0200

    Add get_list_or_404 method

--- taiga/base/api/utils/__init__.py
+++ taiga/base/api/utils/__init__.py
@@ -44,6 +44,7 @@
 
 from django.http import Http404
 from django.shortcuts import get_object_or_404 as _get_object_or_404
+from django.shortcuts import get_list_or_404 as _get_list_or_404
 
 
 def get_object_or_404(queryset, *filter_args, **filter_kwargs):
@@ -53,6 +54,16 @@ def get_object_or_404(queryset, *filter_args, **filter_kwargs):
     """
     try:
         return _get_object_or_404(queryset, *filter_args, **filter_kwargs)
+    except (TypeError, ValueError):
+        raise Http404
+
+def get_list_or_404(queryset, *filter_args, **filter_kwargs):
+    """
+    Same as Django's standard shortcut, but make sure to raise 404
+    if the filter_kwargs don't match the required types.
+    """
+    try:
+        return _get_list_or_404(queryset, *filter_args, **filter_kwargs)
     except (TypeError, ValueError):
         raise Http404
 
